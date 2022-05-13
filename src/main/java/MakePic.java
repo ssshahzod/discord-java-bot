5cbfe38bd98075e106a73723d6ad00c5ac2dedb7
Cronion <ohmytarik@mail.ru>
2022-05-13 23:48:38 +0300

    created new class, to separate meme creation from message sending

---
+++ src/main/java/MakePic.java
@@ -0,0 +1,15 @@
+import java.io.File;
+
+public class MakePic {
+    private String messageText;
+
+    public MakePic(String messageText){
+        this.messageText = messageText;
+    }
+
+    public File makePicture(int typeOfPicture, String messageText){
+
+        return null;
+    }
+
+}
